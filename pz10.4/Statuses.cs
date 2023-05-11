9ed46b29aebd9d64eff99bf8db6787112bff76b7
n1tmer <n1tmer@COMPUTER>
2023-05-11 13:37:38 +0700

    Добавьте файлы проекта.

---
+++ pz10.4/Statuses.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz10._4
+{
+    internal class Statuses
+    {
+        public string Name { get; set; }
+        public Status status { get; set; }
+        public Statuses(string name, Status st)
+        {
+            Name = name;
+            status = st;
+        }
+    }
+    public enum Status
+    {
+        Active,
+        completed
+    }
+}
+
